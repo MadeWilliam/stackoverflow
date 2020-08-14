fd11fc470c281f198cb37709c4ee9d81047cfd1f
Aryandhi Arief Windiarto <57063335+Aryandhi@users.noreply.github.com>
2020-08-14 09:36:39 +0700

    create migration

---
+++ database/migrations/2020_08_14_023111_create_vote_jawaban_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('vote_jawaban');
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('jawaban_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawaban');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vote_jawaban', function (Blueprint $table) {
+            $table->dropColumn('vote_jawaban');
+            $table->dropforeign('user_id');
+            $table->dropforeign('pertanyaan_id');
+        });
+    }
+}
